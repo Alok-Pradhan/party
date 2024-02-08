99f846f2acaf40f4e6364e48984a200bb29f33d9
Alok-Pradhan <alokpradhan196@gmail.com>
2024-02-08 07:31:15 +0530

    added project

---
+++ src/Animatedbg/index.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+export default function ANimatedBg() {
+    return (
+        <>
+            <div class="wrapper">
+                <div class="bg">
+                    {/* <h1>image reveal <br /> on hover</h1> */}
+                </div>
+                <div class="reveal">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ANimatedBg from './Animatedbg';
+
+function App() {
+  return (
+    <div className="App">
+      <ANimatedBg />
+    </div>
+  );
+}
+
+export default App;
